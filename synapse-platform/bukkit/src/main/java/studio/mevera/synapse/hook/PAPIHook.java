406d4bfff1af2dcd02ae91b2746b32de4f76739a
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2025-09-11 19:34:20 +0300

    feat: PAPI compatibiliy with relational placeholders

--- synapse-platform/bukkit/src/main/java/studio/mevera/synapse/hook/PAPIHook.java
+++ synapse-platform/bukkit/src/main/java/studio/mevera/synapse/hook/PAPIHook.java
@@ -1,26 +1,27 @@
 package studio.mevera.synapse.hook;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.clip.placeholderapi.expansion.Relational;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import studio.mevera.synapse.BukkitSynapse;
 import studio.mevera.synapse.context.Context;
 import studio.mevera.synapse.context.type.BasicContex;
+import studio.mevera.synapse.context.type.RelationalContex;
 import studio.mevera.synapse.platform.BukkitNeuron;
 import studio.mevera.synapse.platform.BukkitUser;
 
 import java.util.Arrays;
-import java.util.NoSuchElementException;
 
-public final class PAPIHook extends PlaceholderExpansion {
+public final class PAPIHook extends PlaceholderExpansion implements Relational {
 
     private final BukkitNeuron neuron;
     private final String namespace;
 
     public PAPIHook(final BukkitNeuron neuron) {
         this.neuron = neuron;
-        this.namespace = neuron.namespace().firstName().orElseThrow(() -> new NoSuchElementException("There was no namespace found for a registered neuron"));
+        this.namespace = neuron.namespace().firstName().orElseThrow(() -> new IllegalArgumentException("There was no namespace found for a registered neuron"));
     }
 
     @Override
@@ -49,7 +50,7 @@ public final class PAPIHook extends PlaceholderExpansion {
             final @NotNull String params
     ) {
         if (player == null) {
-            return "";
+            return null;
         }
 
         final BukkitUser user = BukkitSynapse.get().asUser(player);
@@ -64,10 +65,44 @@ public final class PAPIHook extends PlaceholderExpansion {
                 user,
                 tag,
                 this.namespace,
-                Arrays.stream(splitParams).skip(1).toArray(String[]::new)
+                this.skipOne(splitParams)
         );
 
         return this.neuron.onRequest(tag, context);
+    }
+
+    @Override
+    public String onPlaceholderRequest(final Player one, final Player two, final String params) {
+        if (one == null || two == null) {
+            return null;
+        }
+
+        final BukkitUser user = BukkitSynapse.get().asUser(one);
+        final BukkitUser other = BukkitSynapse.get().asUser(two);
+
+        final String[] splitParams = params.split("_");
+        final String tag = splitParams[0];
+
+        if (!this.neuron.isRegistered(tag)) {
+            return null;
+        }
+
+        final Context<BukkitUser> context = new RelationalContex<>(
+                user,
+                other,
+                tag,
+                this.namespace,
+                this.skipOne(splitParams)
+        );
+
+        return this.neuron.onRequest(tag, context);
+    }
+
+    private String[] skipOne(final String[] array) {
+        if (array.length <= 1) {
+            return new String[0];
+        }
+        return Arrays.copyOfRange(array, 1, array.length);
     }
 
 }
