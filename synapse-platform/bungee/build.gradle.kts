0d214552020e136ea1048bcf179524d051d4eca0
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2025-10-04 13:45:31 +0300

    fix: update bungeecord-api dependency version to 1.21-R0.4-SNAPSHOT

--- synapse-platform/bungee/build.gradle.kts
+++ synapse-platform/bungee/build.gradle.kts
@@ -26,7 +26,7 @@ val hiddenShadowed: Configuration by configurations.creating
 dependencies {
     api(project(":synapse-platform:adventure"))
 
-    compileOnly("net.md-5:bungeecord-api:1.20-R0.3-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-api:1.21-R0.4-SNAPSHOT")
     compileOnly("com.alessiodp.libby:libby-bungee:2.0.0-SNAPSHOT")
     hiddenShadowed("com.alessiodp.libby:libby-bungee:2.0.0-SNAPSHOT")
 }
