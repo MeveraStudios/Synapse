e3da14aaab679c06e595c377d026cdda9042f64c
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2025-10-21 14:20:08 +0300

    refactor: optimize argument extraction in RegexUtil and handle empty input

--- synapse-core/src/main/java/studio/mevera/synapse/util/RegexUtil.java
+++ synapse-core/src/main/java/studio/mevera/synapse/util/RegexUtil.java
@@ -12,40 +12,42 @@ public final class RegexUtil {
     }
 
     private final static int INITIAL_CAPACITY = 5; // Initial capacity guess for the list of arguments
+    private static final String[] EMPTY_ARGS = new String[0];
     public static final Pattern PLACEHOLDER_PATTERN = Pattern.compile(
             "\\$\\{([\\w.-]+)((?::\"[^\"]*\"|:'[^']*'|:`[^`]*`|:[^:\\s{}]*?)*)}"
     );
 
     public static String[] extractArguments(final String rawArgs) {
+        if (rawArgs == null || rawArgs.isEmpty()) {
+            return EMPTY_ARGS;
+        }
+
         final List<String> args = new ArrayList<>(INITIAL_CAPACITY);
 
-        int i = 0;
-        while (i < rawArgs.length()) {
-            if (rawArgs.charAt(i) != ':') {
-                i++;
-                continue;
-            }
-            i++; // skip ':'
-            if (i >= rawArgs.length()) break;
+        final int length = rawArgs.length();
 
-            char delimiter = rawArgs.charAt(i);
-            int end;
+        for (int i = 0; i < length; i++) {
+            if (rawArgs.charAt(i) != ':') {
+                continue; // Skip non-delimiter chars
+            }
+
+            if (++i >= length) break; // Skip ':' and check bounds
+
+            final char delimiter = rawArgs.charAt(i);
 
             if (delimiter == '"' || delimiter == '\'' || delimiter == '`') {
-                i++; // skip opening quote
-                end = rawArgs.indexOf(delimiter, i);
+                final int start = ++i; // Skip opening quote
+                final int end = rawArgs.indexOf(delimiter, i);
                 if (end == -1) break;
-                args.add(rawArgs.substring(i, end));
-                i = end + 1;
+                args.add(rawArgs.substring(start, end));
+                i = end; // Will be incremented by loop
             } else {
-                end = i;
-                while (end < rawArgs.length()) {
-                    char c = rawArgs.charAt(end);
-                    if (c == ':' || c == '}' || Character.isWhitespace(c)) break;
-                    end++;
+                final int start = i;
+                while (i < length && rawArgs.charAt(i) != ':') {
+                    i++;
                 }
-                args.add(rawArgs.substring(i, end));
-                i = end;
+                args.add(rawArgs.substring(start, i));
+                i--; // Compensate for loop increment
             }
         }
 
