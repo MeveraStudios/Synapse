06ea21ef2bfc36420ab2b51ea424bfb00e720b69
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2026-03-06 03:08:04 +0200

    feat: implement a fake platform for tests

--- synapse-core/src/test/java/studio/mevera/synapse/impl/TestSynapse.java
+++ synapse-core/src/test/java/studio/mevera/synapse/impl/TestSynapse.java
@@ -1,13 +1,21 @@
 package studio.mevera.synapse.impl;
 
 import studio.mevera.synapse.SynapseBase;
+import studio.mevera.synapse.platform.Platform;
+import studio.mevera.synapse.platform.PlatformInfo;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TestSynapse extends SynapseBase<TestOrigin, TestUser, TestNeuron> {
 
+    private final Platform platform = new PlatformInfo("test", "1.0");
     private final Map<TestOrigin, TestUser> userCache = new ConcurrentHashMap<>();
+
+    @Override
+    public Platform platform() {
+        return platform;
+    }
 
     @Override
     public TestUser asUser(final TestOrigin origin) {
